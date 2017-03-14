fc0397b7ee6cca4fa871b739f66c7dbb4927d387
Paulo Victor <paulovictor.joao@gmail.com>
2017-03-14 11:43:07 -0300

    Controlando os estados do jogo

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
+    public Estado estado { get; private set; }
+
     public GameObject obstaculo;
     public float espera;
     public float tempoDestruicao;
@@ -20,17 +22,26 @@ public class GameController : MonoBehaviour {
     }
 
     void Start () {
-        StartCoroutine(GerarObstaculos());
+        estado = Estado.AguardandoComecar;
 	}
 	
 
 	IEnumerator GerarObstaculos() {
-        while (true) {
-            Vector3 pos = new Vector3(3f, Random.Range(-3f, 3.0f), 0f);
+        while (GameController.instancia.estado == Estado.Jogando) {
+            Vector3 pos = new Vector3(2.1f, Random.Range(-3f, 2.1f), 0f);
             GameObject obj = Instantiate(obstaculo, pos, Quaternion.identity) as GameObject;
             Destroy(obj, tempoDestruicao);
             yield return new WaitForSeconds(espera);
         }
     }
+
+    public void PlayerComecou() {
+        estado = Estado.Jogando;
+        StartCoroutine(GerarObstaculos());
+    }
+
+    public void PlayerMorreu() {
+        estado = Estado.GameOver;
+    } 
 		
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,28 +20,38 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void Update () {
-	    if(Input.GetMouseButtonDown(0)) {
-            anim.Play("pulando");
-            audioSource.PlayOneShot(somPulo);
-            rb.useGravity = true;
-            pulando = true;
+        if (GameController.instancia.estado == Estado.Jogando || GameController.instancia.estado == Estado.AguardandoComecar) {
+            if (Input.GetMouseButtonDown(0)) {
+                anim.Play("pulando");
+                audioSource.PlayOneShot(somPulo);
+                rb.useGravity = true;
+                pulando = true;
+                if (GameController.instancia.estado == Estado.AguardandoComecar) {
+                    GameController.instancia.PlayerComecou();
+                }
+            }
         }
 	}
 
     void FixedUpdate() {
-        if (pulando) {
-            pulando = false;
-            rb.velocity = Vector3.zero;
-            rb.AddForce(Vector3.up * ForcaDoPulo, ForceMode.Impulse);
+        if (GameController.instancia.estado == Estado.Jogando) {
+            if (pulando) {
+                pulando = false;
+                rb.velocity = Vector3.zero;
+                rb.AddForce(Vector3.up * ForcaDoPulo, ForceMode.Impulse);
+            }
         }
     }
 
     void OnCollisionEnter(Collision outro) {
-        if (outro.gameObject.tag == "obstaculo") {
-            rb.AddForce(new Vector3(-50f, 20f, 0f), ForceMode.Impulse);
-            rb.detectCollisions = false;
-            anim.Play("morrendo");
-            audioSource.PlayOneShot(somMorte);
+        if (GameController.instancia.estado == Estado.Jogando) {
+            if (outro.gameObject.tag == "obstaculo") {
+                rb.AddForce(new Vector3(-50f, 20f, 0f), ForceMode.Impulse);
+                rb.detectCollisions = false;
+                anim.Play("morrendo");
+                audioSource.PlayOneShot(somMorte);
+                GameController.instancia.PlayerMorreu();
+            }
         }
     }
 
